2e0a756c68f3a2d36a129aae43fdf350aefb9171
prit29 <pritsahkar2000@gmail.com>
2020-10-14 20:16:19 +0530

    fix navbar icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import {
   Link, Route, Switch
 } from "react-router-dom";
@@ -11,21 +11,6 @@ import home from '../../img/home.png'
 
 function Navbar() {
 
-  const [isPage,setisPage] = useState(true);
-
-  const changeIcon = () => 
-  {
-      if(isPage === false)
-      {
-          setisPage(true);
-      }
-  }
-
-  useEffect(() => 
-  {
-
-  },[isPage]);
-
   function openInNewTab() {
     var win = window.open('https://play.google.com/store/apps/details?id=com.noobsever.codingcontests', '_blank');
     win.focus();
@@ -35,15 +20,18 @@ function Navbar() {
     <div className="Navbar">
       <div className="Header1">
         <img className="Logo" src={logo} alt="logo"/>
-        <Link className="Tt" to="/" onClick={changeIcon}>Coder's Calender</Link>
+        <Link className="Tt" to="/">Coder's Calender</Link>
       </div>
       <div className="Header2">
         <img className="Playstore" onClick={openInNewTab} src={playstore} alt="playstore"/>
-        {isPage ?       
-            <Link to="/settings" onClick={() => setisPage(false)}><img className="St" src={setting} alt="settings"/></Link>
-            :
-            <Link to="/" onClick={() => setisPage(true)}><img className="St" src={home} alt="home"/></Link>
-        }
+          <Switch>
+            <Route exact path="/">
+              <Link to="/settings" ><img className="St" src={setting} alt="settings"/></Link>
+            </Route>
+            <Route exact path="/settings">
+              <Link to="/"><img className="St" src={home} alt="home"/></Link>
+            </Route>
+          </Switch>
       </div>
     </div>
   );
